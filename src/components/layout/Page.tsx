8dbef2cb299e769cab5d58455829ea568d94f1be
Jeff Shuo <jeffshuo@gmail.com>
2022-04-04 10:36:04 +0800

    added notes as a footer for secux users

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,9 +1,9 @@
-import { FC, ReactNode } from "react"
-import classNames from "classnames/bind"
-import { ErrorBoundary, WithFetching } from "../feedback"
-import Container from "./Container"
-import Card from "./Card"
-import styles from "./Page.module.scss"
+import { FC, ReactNode } from 'react'
+import classNames from 'classnames/bind'
+import { ErrorBoundary, WithFetching } from '../feedback'
+import Container from './Container'
+import Card from './Card'
+import styles from './Page.module.scss'
 
 const cx = classNames.bind(styles)
 
@@ -33,13 +33,21 @@ const Page: FC<Props> = ({ title, extra, children, small, sub, ...props }) => {
                 </header>
               )}
 
-              <section className={classNames(styles.main, mainClassName)}>
-                {wrong ? (
-                  <Card>{wrong}</Card>
-                ) : (
-                  <ErrorBoundary>{children}</ErrorBoundary>
-                )}
-              </section>
+              <section className={classNames(styles.main, mainClassName)}>{wrong ? <Card>{wrong}</Card> : <ErrorBoundary>{children}</ErrorBoundary>}</section>
+              <div className={styles.grid}>
+                <strong>Notes:</strong>
+                <ul>
+                  <li>
+                    . This application is a fork of{' '}
+                    <a href="https://chrome.google.com/webstore/detail/terra-station-wallet/aiifbnbfobpmeekipheeijimdpnlpgpp">Terra Station Wallet</a> and
+                    modified for SecuX Wallet and Customers ONLY
+                  </li>
+                  <li>
+                    . To avoid phishing attacks, <strong>NEVER</strong> enter or submit recovery phrase by using this application. Please connect your SecuX
+                    hardware wallet when accessing or transferring your Terra assets
+                  </li>
+                </ul>
+              </div>
             </Container>
           </article>
         </>
